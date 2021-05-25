48bef7863a0dbff1d250f64177054866added304
mujohn26 <mujohn26@gmail.com>
2021-05-25 22:31:08 +0200

    add column names

--- src/App.js
+++ src/App.js
@@ -2,15 +2,13 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Coin from "./Coin";
 import "./App.css";
-import Spinner from './Spinner';
-
+import Spinner from "./Spinner";
 
 function App() {
   const url =
     "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=true&price_change_percentage=1h%2C12h%2C24h%2C7d%2C14d%2C21%2C18d";
   const [coins, setCoins] = useState([]);
   const [search, setSearch] = useState("");
-
 
   useEffect(() => {
     const timer = setTimeout(() => {
@@ -42,23 +40,51 @@ function App() {
           />
         </form>
       </div>
-      { coins.length ? filteredCoins.map((coin) => {
-        return (
-          <Coin
-            key={coin.id}
-            name={coin.name}
-            image={coin.image}
-            symbol={coin.symbol}
-            marketCap={coin.market_cap}
-            price={coin.current_price}
-            priceChange1h={coin.price_change_percentage_1h_in_currency}
-            priceChange24h={coin.price_change_percentage_24h_in_currency}
-            priceChange7d={coin.price_change_percentage_7d_in_currency}
-            priceChange14d={coin.price_change_percentage_14d_in_currency}
-            volume={coin.total_volume}
-          />
-        );
-      }):<Spinner/>}
+      <React.Fragment>
+        <div className="coin-container">
+          <div className="coin_row">
+            <div className="coin">
+              <h1>name</h1>
+              <p className="coin-symbol">symbol</p>
+            </div>
+            <div className="coin-data">
+              <p className="coin_price">price</p>
+              <p className="coin-volume">volume</p>
+
+              <p className="coin-percent ">Change in 1h</p>
+
+              <p className="coin-percent ">Change in 24h</p>
+
+              <p className="coin-percent ">Change in 7d</p>
+
+              <p className="coin-percent "> Change in14d </p>
+
+              <p className="coin-marketcap">Market Capital</p>
+            </div>
+          </div>
+        </div>
+        {coins.length ? (
+          filteredCoins.map((coin) => {
+            return (
+              <Coin
+                key={coin.id}
+                name={coin.name}
+                image={coin.image}
+                symbol={coin.symbol}
+                marketCap={coin.market_cap}
+                price={coin.current_price}
+                priceChange1h={coin.price_change_percentage_1h_in_currency}
+                priceChange24h={coin.price_change_percentage_24h_in_currency}
+                priceChange7d={coin.price_change_percentage_7d_in_currency}
+                priceChange14d={coin.price_change_percentage_14d_in_currency}
+                volume={coin.total_volume}
+              />
+            );
+          })
+        ) : (
+          <Spinner />
+        )}
+      </React.Fragment>
     </div>
   );
 }
--- src/Coin.js
+++ src/Coin.js
@@ -14,46 +14,46 @@ const Coin = ({
   marketCap,
 }) => {
   return (
-    <div className="coin-container">
-      <div className="coin_row">
-        <div className="coin">
-          <img src={image} alt="crypto" />
-          <h1>{name}</h1>
-          <p className="coin-symbol">{symbol}</p>
-        </div>
-        <div className="coin-data">
-          <p className="coin_price">${price.toLocaleString()}</p>
-          <p className="coin-volume">${volume.toLocaleString()}</p>
-          {priceChange1h < 0 ? (
-            <p className="coin-percent red">
-              {priceChange1h && priceChange1h.toFixed(2)}%
-            </p>
-          ) : (
-            <p className="coin-percent green">
-              {priceChange1h && priceChange1h.toFixed(2)}%
-            </p>
-          )}
-          {priceChange24h < 0 ? (
-            <p className="coin-percent red">{priceChange24h.toFixed(2)}%</p>
-          ) : (
-            <p className="coin-percent green">{priceChange24h.toFixed(2)}%</p>
-          )}
-          {priceChange7d < 0 ? (
-            <p className="coin-percent red">{priceChange7d.toFixed(2)}%</p>
-          ) : (
-            <p className="coin-percent green">{priceChange7d.toFixed(2)}%</p>
-          )}
-          {priceChange14d < 0 ? (
-            <p className="coin-percent red">{priceChange14d.toFixed(2)}%</p>
-          ) : (
-            <p className="coin-percent green">{priceChange14d.toFixed(2)}%</p>
-          )}
-          <p className="coin-marketcap">
-            Mkt Cap:$ {marketCap.toLocaleString()}
-          </p>
+    <React.Fragment>
+      <div className="coin-container">
+        <div className="coin_row">
+          <div className="coin">
+            <img src={image} alt="crypto" />
+            <h1>{name}</h1>
+            <p className="coin-symbol">{symbol}</p>
+          </div>
+          <div className="coin-data">
+            <p className="coin_price">${price.toLocaleString()}</p>
+            <p className="coin-volume">${volume.toLocaleString()}</p>
+            {priceChange1h < 0 ? (
+              <p className="coin-percent red">
+                {priceChange1h && priceChange1h.toFixed(2)}%
+              </p>
+            ) : (
+              <p className="coin-percent green">
+                {priceChange1h && priceChange1h.toFixed(2)}%
+              </p>
+            )}
+            {priceChange24h < 0 ? (
+              <p className="coin-percent red">{priceChange24h.toFixed(2)}%</p>
+            ) : (
+              <p className="coin-percent green">{priceChange24h.toFixed(2)}%</p>
+            )}
+            {priceChange7d < 0 ? (
+              <p className="coin-percent red">{priceChange7d.toFixed(2)}%</p>
+            ) : (
+              <p className="coin-percent green">{priceChange7d.toFixed(2)}%</p>
+            )}
+            {priceChange14d < 0 ? (
+              <p className="coin-percent red">{priceChange14d.toFixed(2)}%</p>
+            ) : (
+              <p className="coin-percent green">{priceChange14d.toFixed(2)}%</p>
+            )}
+            <p className="coin-marketcap">$ {marketCap.toLocaleString()}</p>
+          </div>
         </div>
       </div>
-    </div>
+    </React.Fragment>
   );
 };
 
